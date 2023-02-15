75b4ce8ece25dbd83500704768d904131d0e630a
ericknathan <erick.capito@hotmail.com>
2023-02-15 13:40:24 -0300

    feat: insert the global layout in the application

--- src/main/router/router-client.tsx
+++ src/main/router/router-client.tsx
@@ -2,12 +2,19 @@ import {
   createBrowserRouter,
 } from 'react-router-dom';
 
+import { GlobalLayout } from '@presentation/layouts';
 import { Home } from '@presentation/pages';
 
 export const routerClient = createBrowserRouter([
   {
     path: '/',
-    element: <Home />
+    element: <GlobalLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      }
+    ]
   },
 ]);
 
