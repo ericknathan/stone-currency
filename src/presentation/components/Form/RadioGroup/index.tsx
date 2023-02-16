7dcc06ff01530f8dffa8ecf07d793c5e5949c83a
ericknathan <erick.capito@hotmail.com>
2023-02-15 23:09:17 -0300

    feat: add onChange function to radio group component

--- src/presentation/components/Form/RadioGroup/index.tsx
+++ src/presentation/components/Form/RadioGroup/index.tsx
@@ -3,25 +3,31 @@ import { RadioProps } from '@radix-ui/react-radio-group';
 
 import { RadioGroupInput, RadioGroupInputIndicator, RadioGroupInputItem, RadioGroupInputLabel, RadioGroupInputsWrapper, RadioGroupWrapper } from './styles';
 
-interface RadioGroupProps extends RadioProps {
-  name: string;
+interface RadioGroupProps extends Omit<RadioProps, 'onChange'> {
+  name?: string;
   label?: string;
   orientation?: 'vertical' | 'horizontal';
   options: {
     label: string;
     value: string;
-  }[]
+  }[];
+  onChange: (value: string) => void;
 }
 
-export function RadioGroup({ name, label, options, orientation = 'horizontal', ...props }: RadioGroupProps) {
+export function RadioGroup({ name, label, options, orientation = 'horizontal', onChange, ...props }: RadioGroupProps) {
   return (
     <RadioGroupWrapper>
       <FormLabel htmlFor={name}>{label}</FormLabel>
-      <RadioGroupInputsWrapper defaultValue={options[0].value} aria-label={label} orientation={orientation}>
+      <RadioGroupInputsWrapper
+        defaultValue={options[0].value}
+        aria-label={label}
+        orientation={orientation}
+        onValueChange={onChange}
+      >
         {options.map(option => (
           <RadioGroupInput>
             <RadioGroupInputItem {...props} value={option.value} id={option.value}>
-              <RadioGroupInputIndicator />
+              <RadioGroupInputIndicator  />
             </RadioGroupInputItem>
             <RadioGroupInputLabel htmlFor={option.value}>
               {option.label}
