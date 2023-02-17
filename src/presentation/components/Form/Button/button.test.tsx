0d3db298f02f38d176f2fac3186bc5ffd2fd8de1
ericknathan <erick.capito@hotmail.com>
2023-02-17 10:20:09 -0300

    test: ensure button component render disabled styles correctly

--- src/presentation/components/Form/Button/button.test.tsx
+++ src/presentation/components/Form/Button/button.test.tsx
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest';
 import { screen } from '@testing-library/react';
 import 'jest-styled-components';
 
-import { render } from '@test/utils';
+import { render, theme } from '@test/utils';
 import { Button } from '.';
 
 describe("Button Component", () => {
@@ -12,5 +12,13 @@ describe("Button Component", () => {
         render(<Button>{childrenText}</Button>);
 
         expect(screen.getByText(childrenText)).toBeDefined()
+    });
+    
+    it('should render button with disabled styles if it is disabled', () => {
+        render(<Button disabled>{childrenText}</Button>);
+
+        expect(screen.getByRole('button')).toHaveStyleRule('background-color', theme.colors['accent-200'], {
+            modifier: ':disabled'
+        })
     });
 })
