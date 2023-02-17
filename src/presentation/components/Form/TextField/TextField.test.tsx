35258677e203bf42f9e652926b911b6a0e01e3f8
ericknathan <erick.capito@hotmail.com>
2023-02-17 11:08:04 -0300

    test: ensure text field component render placeholder correctly

--- src/presentation/components/Form/TextField/TextField.test.tsx
+++ src/presentation/components/Form/TextField/TextField.test.tsx
@@ -7,10 +7,18 @@ import { TextField } from '.';
 
 describe("TextField Component", () => {
   const label = "Text Field Label";
+  const placeholder = "Text Field Placeholder";
 
   it("should render a text field with correct label", async () => {
     render(<TextField label={label} name="example" />);
 
     expect(screen.getByLabelText(label)).toBeDefined();
   });
+
+  it("should render a text field with correct placeholder", async () => {
+    render(<TextField placeholder={placeholder} name="example" />);
+
+    expect(screen.getByPlaceholderText(placeholder)).toBeDefined();
+  });
+
 })
