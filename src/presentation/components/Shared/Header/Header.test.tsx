cdac37b6c7c7040f92b74c73820a9b67bf957f4e
ericknathan <erick.capito@hotmail.com>
2023-02-17 13:41:36 -0300

    test: ensure header component render correct info when data is loaded

--- src/presentation/components/Shared/Header/Header.test.tsx
+++ src/presentation/components/Shared/Header/Header.test.tsx
@@ -5,6 +5,7 @@ import 'jest-styled-components';
 
 import * as queries from '@infra/cache/queries';
 import { mockQuery, render } from '@test/utils';
+import { formatDate, formatTime } from '@main/utils';
 import { Header } from '.';
 
 describe("Header Component", () => {
@@ -18,5 +19,19 @@ describe("Header Component", () => {
     render(<Header />);
 
     expect(screen.getByTestId('skeleton')).toBeDefined();
+  });
+
+  it("should render a header with correct info when data is loaded", async () => {
+    vi.spyOn(queries, 'useLastCurrencyQuotationQuery').mockImplementation(() => mockQuery({
+      requestTimestamp: new Date().getTime()
+    }));
+    
+    render(<Header />);
+
+    const requestTimestampDate = new Date(new Date().getTime());
+    const formattedDate = formatDate(requestTimestampDate);
+    const formattedTime = formatTime(requestTimestampDate);
+
+    expect(screen.getByText(`${formattedDate} | ${formattedTime} UTC`)).toBeDefined();
   });
 });
