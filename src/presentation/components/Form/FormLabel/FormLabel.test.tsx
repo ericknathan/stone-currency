6272afa7e2d0cb80f21b0d9ef3f9c2876dec375f
ericknathan <erick.capito@hotmail.com>
2023-02-17 10:23:55 -0300

    test: ensure form label component render passed text correctly

---
+++ src/presentation/components/Form/FormLabel/FormLabel.test.tsx
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest';
+import { screen } from '@testing-library/react';
+import 'jest-styled-components';
+
+import { render } from '@test/utils';
+import { FormLabel } from '.';
+
+describe("FormLabel Component", () => {
+    const labelText = "Label example";
+
+    it("should render a form label with correct text", () => {
+        render(<FormLabel>{labelText}</FormLabel>);
+
+        expect(screen.getByText(labelText)).toBeDefined()
+    });
+})
