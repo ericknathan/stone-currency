9af1640e8c8013fc661116274d76634cbc602f9a
ericknathan <erick.capito@hotmail.com>
2023-02-16 23:15:30 -0300

    feat: create currency converters

---
+++ src/main/utils/currency.util.ts
@@ -0,0 +1,34 @@
+interface CalculateProps {
+  currency: number;
+  statePercentageTax: number;
+  nowCurrencyValue: number;
+}
+
+export function getDecimalPercentage(percentage: number) {
+  return 1 + (percentage / 100);
+}
+
+export function calculateMoney(data: CalculateProps) {
+  const { currency, statePercentageTax, nowCurrencyValue } = data;
+  const purchaseIOF = 1.1;
+
+  console.log({ currency, statePercentageTax, nowCurrencyValue, purchaseIOF })
+
+  return (currency * getDecimalPercentage(statePercentageTax)) * (nowCurrencyValue * getDecimalPercentage(purchaseIOF));
+}
+
+export function calculateCard(data: CalculateProps) {
+  const { currency, statePercentageTax, nowCurrencyValue } = data;
+  const purchaseIOF = 6.4;
+  
+  return (currency * getDecimalPercentage(statePercentageTax) * getDecimalPercentage(purchaseIOF)) * nowCurrencyValue;
+}
+
+export function formatMoney(value: number, currency: 'USD' | 'BRL' = 'BRL') {
+  const formatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency,
+  });
+
+  return formatter.format(value);
+}
--- src/main/utils/index.ts
+++ src/main/utils/index.ts
@@ -1,1 +1,2 @@
+export * from './currency.util';
 export * from './date.util';
